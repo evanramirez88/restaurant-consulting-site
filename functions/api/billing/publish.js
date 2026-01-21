e004c5723204c15b37add7f8c894a31ad063c5c2
R&G Consulting <ramirezconsulting.rg@gmail.com>
2026-01-21 16:55:40 -0500

    Trigger deploy for WORKER_API_KEY env sync

--- functions/api/billing/publish.js
+++ functions/api/billing/publish.js
@@ -171,3 +171,4 @@ export async function onRequestOptions() {
 // Trigger redeploy Wed, Jan 21, 2026  3:58:37 PM
 // Redeploy after env update Wed, Jan 21, 2026  4:01:56 PM
 // Final env sync Wed, Jan 21, 2026  4:07:46 PM
+// WORKER_API_KEY sync Wed, Jan 21, 2026  4:43:00 PM
